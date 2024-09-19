8bc0d9424a8789cd96f2b454188fe624a69ae578
Mechanikum <onemoremechmail@gmail.com>
2024-09-19 10:20:43 +0700

    Fixed adaptive version

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {ChevronsDown} from "lucide-react";
 
 function App() {
     const {scrollYProgress} = useScroll();
-    const width = useTransform(scrollYProgress, [0, 0.2, 0.7, 1], ["0%", "100%", "100%", "0%"]);
+    const width = useTransform(scrollYProgress, [0, 0.2, 0.7, 0.95], ["0%", "100%", "100%", "0%"]);
     const opacity = useTransform(scrollYProgress, [0, 0.15, 0.2], ["100%", "50%", "0%"]);
 
     const skewX = useTransform(scrollYProgress, [0, 0.2], ["-12deg", "12deg"]);
@@ -12,20 +12,24 @@ function App() {
     const textOffset = useTransform(scrollYProgress, [0, 0.22, 0.36], ["0%", "0%", "100%"]);
 
     const imageFrame = {
-        width: useTransform(scrollYProgress, [0.36, 0.5, 0.7], ["0", "20%", "100%"]),
-        height: useTransform(scrollYProgress, [0.36, 0.5, 0.7], ["0", "20%", "100%"]),
-        rotate: useTransform(scrollYProgress, [0.36, 0.5, 0.7], ["0deg", "180deg", "360deg"]),
+        width: useTransform(scrollYProgress, [0.36, 0.68], ["max(0vh,0vw)", "max(100vh,100vw)"]),
+        height: useTransform(scrollYProgress, [0.36, 0.68], ["max(0vh,0vw)", "max(100vh,100vw)"]),
+        rotate: useTransform(scrollYProgress, [0.36, 0.68], ["0deg", "180deg"]),
     }
 
-    const imageCounterRotate = useTransform(scrollYProgress, [0.36, 0.5, 0.7], ["-0deg", "-180deg", "-360deg"]);
+    const imageCounterTransform = {
+        rotate: useTransform(scrollYProgress, [0.36, 0.68], ["0deg", "-180deg"]),
+    }
 
     const videoFrame = {
-        width: useTransform(scrollYProgress, [0.55, 0.7, 1], ["0", "20%", "100%"]),
-        height: useTransform(scrollYProgress, [0.55, 0.7, 1], ["0", "20%", "100%"]),
-        skewX: useTransform(scrollYProgress, [0.55, 0.8, 1], ["-80deg", "-40deg", "0deg"])
+        translateX:  useTransform(scrollYProgress, [0.65, 0.7, 1], ["120%","120%", "0%"]),
+        skewX: useTransform(scrollYProgress, [0.6, 0.95, 1], ["-30deg", "-30deg", "0deg"])
     }
 
-    const videoCounterSkewX = useTransform(scrollYProgress, [0.6, 0.8, 1], ["80deg", "40deg", "0deg"])
+    const videoCounterTransform = {
+        skewX: useTransform(scrollYProgress, [0.6, 0.95, 1], ["30deg", "30deg", "0deg"]),
+        left: useTransform(scrollYProgress, [0.6, 0.8, 1], ["-50%", "0", "50%"]),
+    }
 
     const finishedScrollers = {
         opacity: useTransform(scrollYProgress, [0.8, 1], ["0", "100%"]),
@@ -41,7 +45,7 @@ function App() {
                     </div>
                 </motion.div>
             </div>
-            <div className={"fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl uppercase overflow-hidden flex flex-row px-16"}>
+            <div className={"fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl sm:text-2xl uppercase overflow-hidden flex flex-row px-16"}>
                 <motion.div
                     style={{right: textOffset}}
                     className={"relative pl-4 py-2 overflow-hidden"}
@@ -50,7 +54,7 @@ function App() {
                         style={{skewX: skewX, backgroundColor: skewColor}}
                         className={"absolute w-full h-full z-[-1] top-0 left-1.5 skew-x-12"}
                     />
-                    <p>
+                    <p className={"text-nowrap"}>
                         I am here to bl
                     </p>
                 </motion.div>
@@ -62,7 +66,7 @@ function App() {
                         style={{skewX: skewX, backgroundColor: skewColor}}
                         className={"absolute w-full h-full z-[-1] top-0 right-1.5 skew-x-12"}
                     />
-                    <p>
+                    <p className={"text-nowrap"}>
                         ow your mind
                     </p>
                 </motion.div>
@@ -76,22 +80,22 @@ function App() {
                     className={"overflow-hidden relative"}
                 >
                     <motion.img
-                        style={{rotate: imageCounterRotate}}
-                        className={"absolute top-1/2 left-1/2 [translate:-50%_-50%] w-screen h-auto max-w-none"}
+                        style={imageCounterTransform}
+                        className={"absolute top-1/2 left-1/2 [translate:-50%_-50%] min-w-screen min-h-screen h-auto max-w-none"}
                         src={"/image.webp"}
                     />
                 </motion.div>
             </div>
-            <div className={"fixed z-[-1] w-screen h-screen flex items-center justify-center"}>
+            <div className={"fixed z-[-1] justify-center"}>
                 <motion.div
                     style={videoFrame}
-                    className={"overflow-hidden relative"}
+                    className={"overflow-hidden fixed top-1/2 left-1/2 [translate:-50%_-50%] w-[max(100vw,100vh)] h-[max(100vw,100vh)]"}
                 >
                     <motion.video
                         width="1920"
                         height="1080"
-                        style={{skew: videoCounterSkewX}}
-                        className={"absolute top-1/2 left-1/2 [translate:-50%_-50%] w-screen h-auto max-w-none"}
+                        style={videoCounterTransform}
+                        className={"absolute top-1/2 left-1/2 [translate:-50%_-50%] min-w-screen min-h-screen h-auto max-w-none"}
                         autoPlay={true}
                         loop={true}
                         muted={true}
@@ -106,25 +110,25 @@ function App() {
             <div className={"fixed z-0 w-screen h-screen overflow-hidden"}>
                 <motion.div
                     style={finishedScrollers}
-                    className={"absolute bottom-1/2 -right-1/2 rotate-45 w-screen bg-pink-600 overflow-hidden text-lg"}
+                    className={"absolute top-0 right-0 [translate:30%_-30%] rotate-45 w-[max(100vw,100vh)] bg-pink-600 overflow-hidden text-lg"}
                 >
                     <TextScroller direction={"rtl"} text={"DEMO_FINISHED_"}/>
                 </motion.div>
                 <motion.div
                     style={finishedScrollers}
-                    className={"absolute bottom-1/2 right-1/2 rotate-45 w-screen bg-pink-600 overflow-hidden text-lg"}
+                    className={"absolute bottom-0 left-0 [translate:-30%_30%] rotate-45 w-[max(100vw,100vh)] bg-pink-600 overflow-hidden text-lg"}
                 >
                     <TextScroller direction={"rtl"} text={"NO_FOR_REAL_"}/>
                 </motion.div>
                 <motion.div
                     style={finishedScrollers}
-                    className={"absolute bottom-1/2 right-1/2 -rotate-45 w-screen bg-pink-600 overflow-hidden text-lg"}
+                    className={"absolute top-0 left-0 [translate:-30%_-30%] -rotate-45 w-[max(100vw,100vh)] bg-pink-600 overflow-hidden text-lg"}
                 >
                     <TextScroller direction={"rtl"} text={"THATS_ALL_"}/>
                 </motion.div>
                 <motion.div
                     style={finishedScrollers}
-                    className={"absolute bottom-1/2 -right-1/2 -rotate-45 w-screen bg-pink-600 overflow-hidden text-lg"}
+                    className={"absolute bottom-0 right-0 [translate:30%_30%] -rotate-45 w-[max(100vw,100vh)] bg-pink-600 overflow-hidden text-lg"}
                 >
                     <TextScroller direction={"rtl"} text={"I_SPENT_5_HOURS_ON_THIS_HELP_"}/>
                 </motion.div>
